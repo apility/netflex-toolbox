905facb149f923da680596f9f0f9284aa290c44a
Tommy Stigen Olsen <tommysolsen@gmail.com>
2023-12-08 17:01:22 +0100

    Add authentication to webhook endpoint

--- src/Http/Controllers/StructureWebhookController.php
+++ src/Http/Controllers/StructureWebhookController.php
@@ -6,7 +6,22 @@ use Netflex\Toolbox\Events\Structures\StructureEvent;
 
 class StructureWebhookController
 {
-    public function process() {
+    public function process()
+    {
+
+        $messageId = request()->header('x-message-id');
+        $digest = request()->header('x-message-digest');
+
+        $potentialKeys = collect(config('api.connections', []))
+            ->pluck('privateKey');
+        $potentialKeys->add(config('api.privateKey'));
+        $validDigest = $potentialKeys
+                ->filter()
+                ->filter(fn($key) => hash('sha256', $messageId . $key) === $digest)
+                ->count() > 0;
+
+        abort_unless($validDigest, 403);
+
         $event = StructureEvent::fromRequest();
         event($event);
         return "OK";
